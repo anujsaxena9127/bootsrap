82a247193224ff85d431c610c30d10011b5ddae2
Anuj <anujsaxena9127@gmail.com>
2019-08-01 00:36:37 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,13 @@
+
+ $("p").on({
+  mouseenter: function(){
+    $(this).css("background-color", "lightgray");
+  },
+  mouseleave: function(){
+    $(this).css("background-color", "lightblue");
+  },
+  click: function(){
+    $(this).css("background-color", "yellow");
+  }
+}); 
+
